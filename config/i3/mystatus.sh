78d1904e9d3f2c3734f99b881b0eea25dde1461c
Stefan Wimmer <wimstefan@gmail.com>
2017-01-08 17:23:10 +0100

    i3: removed since I'll stick with awesome

--- config/i3/mystatus.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-i3status -c ~/.i3/i3status.conf | while :
-do
-    read line
-    id=$(xprop -root | awk '/_NET_ACTIVE_WINDOW\(WINDOW\)/{print $NF}')
-    name=$(xprop -id $id | awk '/_NET_WM_NAME/{$1=$2="";print}' | cut -d'"' -f2)
-    echo "$name | $line" || exit 1
-done
