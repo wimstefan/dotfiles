5ca4a00abe29f863c9b679b35f1045d990b8dd14
Stefan Wimmer <wimstefan@gmail.com>
2021-04-07 13:28:53 +0200

    [neovim] vim.inspect made easy :)

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -673,4 +673,10 @@ augroup RC
 augroup END
 ]], false)
 -- }}}1 --------------------- AUTOCMDS -----------------------------------------
+-- {{{1 --------------------- FUNCTIONS ----------------------------------------
+function _G.inspect(...)
+  local objects = vim.tbl_map(vim.inspect, {...})
+  print(unpack(objects))
+end
+-- }}}1 --------------------- FUNCTIONS ----------------------------------------
 -- vim: foldmethod=marker foldlevel=0
