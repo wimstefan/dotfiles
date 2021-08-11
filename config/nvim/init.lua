0ee2a944af6cb833fd5161f22aea833405bfe0a3
Stefan Wimmer <wimstefan@gmail.com>
2021-08-11 11:34:36 +0200

    [neovim] LSP configuration tweaks: enhance textDocument capabilities for nvim-cmp

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -764,7 +764,14 @@ use {
       local function make_config()
         local capabilities = vim.lsp.protocol.make_client_capabilities()
         capabilities = vim.tbl_extend('keep', capabilities, lsp_status.capabilities)
+        capabilities.textDocument.completion.completionItem.documentationFormat = { 'markdown' }
         capabilities.textDocument.completion.completionItem.snippetSupport = true
+        capabilities.textDocument.completion.completionItem.preselectSupport = true
+        capabilities.textDocument.completion.completionItem.insertReplaceSupport = true
+        capabilities.textDocument.completion.completionItem.labelDetailsSupport = true
+        capabilities.textDocument.completion.completionItem.deprecatedSupport = true
+        capabilities.textDocument.completion.completionItem.commitCharactersSupport = true
+        capabilities.textDocument.completion.completionItem.tagSupport = { valueSet = { 1 } }
         capabilities.textDocument.completion.completionItem.resolveSupport = {
           properties = {
             'documentation',
