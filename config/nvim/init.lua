4ccd1fe35bbf4d79d1b90a003baccd95bb5d78e0
Stefan Wimmer <wimstefan@gmail.com>
2021-09-13 11:17:11 +0200

    [neovim] Plugin configuration tweaks: bootstrap packer.nvim with minimal repository size

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -285,7 +285,7 @@ end
 -- {{{1 --------------------- PLUGINS ------------------------------------------
 local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/opt/packer.nvim'
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  vim.fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 end
 
 vim.api.nvim_command[[packadd packer.nvim]]
