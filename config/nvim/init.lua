7d8c0b479321af01a12319d90aa5f80d519ae6d4
Stefan Wimmer <wimstefan@gmail.com>
2021-11-21 01:52:26 +0100

    [neovim] simplify 'inspect' function

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -268,13 +268,7 @@ augroup END
 -- }}}1 --------------------- AUTOCMDS -----------------------------------------
 -- {{{1 --------------------- FUNCTIONS ----------------------------------------
 function _G.inspect(...)
-  local objects = {}
-  for i = 1, select('#', ...) do
-    local v = select(i, ...)
-    table.insert(objects, vim.inspect(v))
-  end
-  vim.notify(table.concat(objects, '\n'), vim.log.levels.INFO, {title = '[Inspect]'})
-  return ...
+  vim.notify(vim.inspect(...))
 end
 -- }}}1 --------------------- FUNCTIONS ----------------------------------------
 -- {{{1 --------------------- PLUGINS ------------------------------------------
