8af96fd14f50fc4cd4bf7e7ea42290a883eed90b
Stefan Wimmer <wimstefan@gmail.com>
2022-03-15 12:56:11 +0100

    [neovim] Treesitter: * refactor: remove default configuration

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -503,22 +503,8 @@ packer.startup(function()
         refactor = {
           highlight_current_scope = { enable = false },
           highlight_definitions = { enable = true },
-          navigation = {
-            enable = true,
-            keymaps = {
-              goto_definition = 'gnd',
-              list_definitions = 'gnD',
-              list_definitions_toc = 'gO',
-              goto_next_usage = '<a-*>',
-              goto_previous_usage = '<a-#>',
-            },
-          },
-          smart_rename = {
-            enable = true,
-            keymaps = {
-              smart_rename = 'grr',
-            },
-          },
+          navigation = { enable = true },
+          smart_rename = { enable = true },
         },
         textobjects = {
           lookahead = true,
