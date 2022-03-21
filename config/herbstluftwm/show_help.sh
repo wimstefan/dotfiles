d4f4ff456adc1b3663bf21771747d1a390669bab
Stefan Wimmer <wimstefan@gmail.com>
2022-03-21 16:20:01 +0100

    [herbstluftwm] * add tabbed_max setting * consistent use of $HOME variable and class names * use official picom * tweak dropdown terminals position & geometry * tweak keybinding script

--- config/herbstluftwm/show_help.sh
+++ config/herbstluftwm/show_help.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
-herbstclient list_keybinds | fzf --height=100% --ansi
-cat ~/.config/herbstluftwm/sxhkdrc | awk '/^[a-z]/ && last {print $0,"\t",last} {last=""} /^#/{last=$0}' | column -t -s $'\t' | fzf --height=100% --ansi
+herbstclient list_keybinds | fzf --height=100%
+cat ~/.config/herbstluftwm/sxhkdrc | awk '/^[a-z]/ && last {print $0,"\t",last} {last=""} /^#/{last=$0}' | column -t -s $'\t' | fzf --height=100%
