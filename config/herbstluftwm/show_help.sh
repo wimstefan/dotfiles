72c431ffc17dbe81116c05a799f521bc5e687ec2
Stefan Wimmer <wimstefan@gmail.com>
2022-04-28 14:31:14 +0200

    [herbstluftwm] use global script to show keybindings

--- config/herbstluftwm/show_help.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/zsh
-herbstclient list_keybinds | fzf --height=100%
-cat ~/.config/herbstluftwm/sxhkdrc | awk '/^[a-z]/ && last {print $0,"\t",last} {last=""} /^#/{last=$0}' | column -t -s $'\t' | fzf --height=100%
