0ee7555808218ab77d17a3aa84da796a0d5b5046
Stefan Wimmer <wimstefan@gmail.com>
2022-12-30 12:25:20 +0100

    [neovim] lazy: apply upstream configuration changes

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -15,7 +15,7 @@ if not vim.loop.fs_stat(lazypath) then
     lazypath,
   })
 end
-vim.opt.runtimepath:prepend(lazypath)
+vim.opt.rtp:prepend(lazypath)
 -- }}}
 -- {{{1 --------------------- OPTIONS ------------------------------------------
 local indent = 2
