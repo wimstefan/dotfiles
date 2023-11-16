3df25269ecd030b5cc1b7826ecbf1d8a2ac54fa0
Stefan Wimmer <wimstefan@gmail.com>
2023-11-16 16:31:13 +0100

    [neovim] options: * clipboard: add upstream updates to the configuration

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -3,12 +3,12 @@ local indent = 2
 vim.g.clipboard = {
   name = 'OSC 52',
   copy = {
-    ['+'] = require('vim.clipboard.osc52').copy,
-    ['*'] = require('vim.clipboard.osc52').copy,
+    ['+'] = require('vim.ui.clipboard.osc52').copy,
+    ['*'] = require('vim.ui.clipboard.osc52').copy,
   },
   paste = {
-    ['+'] = require('vim.clipboard.osc52').paste,
-    ['*'] = require('vim.clipboard.osc52').paste,
+    ['+'] = require('vim.ui.clipboard.osc52').paste,
+    ['*'] = require('vim.ui.clipboard.osc52').paste,
   },
 }
 
