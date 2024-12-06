8514ca8d3c4b1ee6b3ddb3fc91ad316b147761c5
Stefan Wimmer <wimstefan@gmail.com>
2024-12-06 12:25:56 +0100

    [neovim] misc: snacks.nvim * explicitely enable needed plugins * add dashboard * add scratch buffer * little tweaks

--- config/nvim/lua/plugins/misc.lua
+++ config/nvim/lua/plugins/misc.lua
@@ -2,8 +2,35 @@ return {
   'nvim-lua/plenary.nvim',
   {
     'folke/snacks.nvim',
-    event = 'VeryLazy',
+    lazy = false,
+    priority = 1000,
     opts = {
+      bigfile = { enabled = true },
+      dashboard = {
+        enabled = true,
+        width = math.floor(vim.o.columns * 0.44),
+        sections = {
+          { section = 'header', padding = 2 },
+          { section = 'keys', padding = 2 },
+          { section = 'recent_files', icon = ' ', title = 'Recent Files', indent = 3, padding = 2 },
+          { section = 'projects', icon = ' ', title = 'Projects', indent = 3, padding = 2 },
+          {
+            section = 'terminal',
+            enabled = function()
+              return Snacks.git.get_root() ~= nil
+            end,
+            icon = ' ',
+            title = 'Git Status',
+            height = 8,
+            padding = 2,
+            indent = 0,
+            cmd = 'hub --no-pager diff --stat -B -M -C'
+          },
+          { section = 'startup' }
+        }
+      },
+      notifier = { enabled = true },
+      quickfile = { enabled = true },
       statuscolumn = {
         enabled = true,
         folds = {
@@ -11,6 +38,7 @@ return {
           git_hl = true
         }
       },
+      words = { enabled = true },
       styles = {
         terminal = {
           position = 'float',
@@ -20,7 +48,10 @@ return {
       win = { border = require('config.ui').borders }
     },
     keys = {
+      { ',sd', function() Snacks.dashboard() end, desc = 'Snacks: dashboard' },
       { ',sr', function() Snacks.rename.rename_file() end, desc = 'Snacks: rename file' },
+      { ',ss', function() Snacks.scratch() end, desc = 'Toggle Scratch Buffer' },
+      { ',sS', function() Snacks.scratch.select() end, desc = 'Select Scratch Buffer' },
       { '<Leader>bd', function() Snacks.bufdelete() end, desc = 'Snacks: delete buffer' },
       { '<Leader>L', function() Snacks.lazygit() end, desc = 'Lazygit' },
       { '<Leader>gf', function() Snacks.lazygit.log_file() end, desc = 'Lazygit Current File History' },
@@ -33,6 +64,13 @@ return {
       vim.api.nvim_create_autocmd('User', {
         pattern = 'VeryLazy',
         callback = function()
+          _G.dd = function(...)
+            Snacks.debug.inspect(...)
+          end
+          _G.bt = function()
+            Snacks.debug.backtrace()
+          end
+          vim.print = _G.dd
           Snacks.toggle.option('background', { off = 'light', on = 'dark', name = 'background' }):map('yob')
           Snacks.toggle.option('hlsearch', { name = 'search highlighting' }):map('yoh')
           Snacks.toggle.option('spell', { name = 'spelling' }):map('yos')
