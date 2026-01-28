1774ac14b7c0fcaaf342dc442f57ca92c9732d49
Stefan Wimmer <wimstefan@gmail.com>
2026-01-28 13:09:13 +0100

    [neovim] treesitter: add asciidoc parsers for markview.nvim

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -55,6 +55,28 @@ vim.api.nvim_create_autocmd('FileType', {
     vim.bo[bufnr].syntax = 'on'
   end
 })
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'TSUpdate',
+  callback = function()
+    require('nvim-treesitter.parsers').asciidoc = {
+      install_info = {
+        branch = 'master',
+        location = 'tree-sitter-asciidoc',
+        queries = 'queries/asciidoc/',
+        requires = { 'asciidoc_inline' },
+        url = 'https://github.com/cathaysia/tree-sitter-asciidoc'
+      }
+    }
+    require('nvim-treesitter.parsers').asciidoc_inline = {
+      install_info = {
+        branch = 'master',
+        location = 'tree-sitter-asciidoc_inline',
+        queries = 'queries/asciidoc_inline',
+        url = 'https://github.com/cathaysia/tree-sitter-asciidoc'
+      }
+    }
+  end
+})
 
 vim.pack.add({
   {
