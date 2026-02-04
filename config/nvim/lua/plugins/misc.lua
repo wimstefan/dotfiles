138fa0e3c9d496ee8989332b960994d3d99b1250
Stefan Wimmer <wimstefan@gmail.com>
2026-02-04 13:20:37 +0100

    [neovim] misc: use yazi for all explorer activity

--- config/nvim/lua/plugins/misc.lua
+++ config/nvim/lua/plugins/misc.lua
@@ -9,12 +9,18 @@ vim.pack.add({
 vim.pack.add({
   { src = 'https://github.com/mikavilpas/yazi.nvim' }
 })
-require('yazi').setup({
-  yazi_floating_window_border = require('config.ui').borders
+vim.g.loaded_netrwPlugin = 1
+vim.api.nvim_create_autocmd('UiEnter', {
+  callback = function()
+    require('yazi').setup({
+      open_for_directories = true,
+      yazi_floating_window_border = require('config.ui').borders
+    })
+    vim.keymap.set('n', '<Leader>y', '<Cmd>Yazi<CR>', { desc = 'Open yazi at the current file' })
+    vim.keymap.set('n', '<Leader>yw', '<Cmd>Yazi cwd<CR>', { desc = "Open the file manager in nvim's working directory" })
+    vim.keymap.set('n', ',ty', '<Cmd>Yazi toggle<CR>', { desc = 'Resume the last yazi session' })
+  end
 })
-vim.keymap.set('n', '<Leader>y', '<Cmd>Yazi<CR>', { desc = 'Open yazi at the current file' })
-vim.keymap.set('n', '<Leader>yw', '<Cmd>Yazi cwd<CR>', { desc = "Open the file manager in nvim's working directory" })
-vim.keymap.set('n', ',ty', '<Cmd>Yazi toggle<CR>', { desc = 'Resume the last yazi session' })
 
 vim.pack.add({
   { src = 'https://github.com/benoror/gpg.nvim' }
