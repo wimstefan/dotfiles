895721f6c7f3f53e73d5a1b5a47b47413344439d
Stefan Wimmer <wimstefan@gmail.com>
2026-04-27 12:30:32 +0200

    [neovim] ui: move `LspProgress` autocmd

--- config/nvim/lua/plugins/snacks.lua
+++ config/nvim/lua/plugins/snacks.lua
@@ -107,44 +107,6 @@ vim.keymap.set({ 'n', 't' }, '<C-\\>', function() Snacks.terminal.toggle() end, 
 vim.keymap.set({ 'n', 't' }, ']]', function() Snacks.words.jump(vim.v.count1) end, { desc = 'Snacks: next reference' })
 vim.keymap.set({ 'n', 't' }, '[[', function() Snacks.words.jump(-vim.v.count1) end, { desc = 'Snacks: prev reference' })
 
-local progress = vim.defaulttable()
-vim.api.nvim_create_autocmd('LspProgress', {
-  callback = function(ev)
-    local client = vim.lsp.get_client_by_id(ev.data.client_id)
-    local value = ev.data.params.value
-    if not client or type(value) ~= 'table' then
-      return
-    end
-    local p = progress[client.id]
-    for i = 1, #p + 1 do
-      if i == #p + 1 or p[i].token == ev.data.params.token then
-        p[i] = {
-          token = ev.data.params.token,
-          msg = ('[%3d%%] %s%s'):format(
-            value.kind == 'end' and 100 or value.percentage or 100,
-            value.title or '',
-            value.message and (' **%s**'):format(value.message) or ''
-          ),
-          done = value.kind == 'end',
-        }
-        break
-      end
-    end
-    local msg = {}
-    progress[client.id] = vim.tbl_filter(function(v)
-      return table.insert(msg, v.msg) or not v.done
-    end, p)
-    local spinner = { '⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏' }
-    vim.notify(table.concat(msg, '\n'), 'info', {
-      id = 'lsp_progress',
-      title = client.name,
-      opts = function(notif)
-        notif.icon = #progress[client.id] == 0 and ' '
-          or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
-      end
-    })
-  end
-})
 vim.api.nvim_create_autocmd('User', {
   callback = function()
     _G.dd = function(...)
@@ -161,6 +123,6 @@ vim.api.nvim_create_autocmd('User', {
     Snacks.toggle.option('relativenumber', { name = 'relative number' }):map('yor')
     Snacks.toggle.line_number({ name = 'absolute number' }):map('yol')
     Snacks.toggle.indent({ name = 'indent' }):map(',ti')
-    Snacks.toggle.scroll({ name = 'scroll' }):map(',ts')
+    -- Snacks.toggle.scroll({ name = 'scroll' }):map(',ts')
   end
 })
