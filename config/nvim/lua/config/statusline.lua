79d0fdcf4bbcb240994b7e1836af81c9bec06fde
Stefan Wimmer <wimstefan@gmail.com>
2026-09-03 11:53:44 +0200

    [neovim] statusline: use `v:hlsearch` instead of `searchcount().total`

--- config/nvim/lua/config/statusline.lua
+++ config/nvim/lua/config/statusline.lua
@@ -53,7 +53,7 @@ end
 
 --- @return string
 local function search_count()
-  return '%#Bold#' .. '%{searchcount().total > 0 ? "(" . searchcount().current . "/" . searchcount().total . ") " : ""}' .. '%*'
+  return '%#Bold#' .. '%{v:hlsearch ? "(" . searchcount().current . "/" . searchcount().total . ") " : ""}' .. '%*'
 end
 
 --- @param type string
