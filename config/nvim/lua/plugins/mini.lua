e7aa79c220265b8a92fe78b6a0a91903c12c9b45
Stefan Wimmer <wimstefan@gmail.com>
2026-09-12 16:25:33 +0200

    [neovim] mini: fix EmmyLua error

--- config/nvim/lua/plugins/mini.lua
+++ config/nvim/lua/plugins/mini.lua
@@ -17,7 +17,7 @@ vim.pack.add({
   { src = 'https://github.com/nvim-mini/mini.icons' }
 })
 require('mini.icons').setup()
-MiniIcons.mock_nvim_web_devicons()
+require('mini.icons').mock_nvim_web_devicons()
 
 vim.pack.add({
   { src = 'https://github.com/nvim-mini/mini.indentscope' }
